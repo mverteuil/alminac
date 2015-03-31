1d112ac7719cb2f787c2f62f5066d10749c6c511
M. de Verteuil <onceuponajooks@gmail.com>
2015-03-31 16:03:01 -0400

    Added recipe mining tool

---
+++ tools/recipe_mining.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Minecraft 1.8 recipe mining tool
+# Author: Matthew de Verteuil (http://www.github.com/mverteuil)
+
+# Extract the individual recipes from CraftingManager.java...
+grep -Eo "add(ShapelessRecipe|Recipe)(?:\()(new ItemStack.*?\)+).*"             \
+    parts/mcp910/src/minecraft/net/minecraft/item/crafting/CraftingManager.java |
+# ... and sort it
+sort
